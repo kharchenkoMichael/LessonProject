09bc5c6d0a52246694c2eaa8a0e7141c94cc595c
ZAMLER <jekaninho7@gmail.com>
2022-10-25 17:45:48 +0300

    #3 registration

--- ProjectLesson/UserInterface/Program.cs
+++ ProjectLesson/UserInterface/Program.cs
@@ -1,1 +1,68 @@
 ﻿
+
+using BussinesLogic;
+using Storage;
+
+void Enter()
+{ 
+ //TODO : доделать вход
+}
+var userService = new UserService();
+while (true)
+{
+    Console.WriteLine("Выберите команду");
+    Console.WriteLine("1 - вход");
+    Console.WriteLine("2 - Регистрация");
+
+    var command = Console.ReadLine();
+    switch (command)
+    {
+        case "1":
+            Enter();
+            break;
+        case "2":
+            Registration();
+            break;
+        default:
+            Console.WriteLine("Нет такой команды,попробуйте еще");
+            break;
+
+
+    }
+
+}
+void Registration()
+{
+    string phone = "";
+    string password = "";
+    while (true)
+    {
+        Console.WriteLine("Введите свой номер телефона");
+        phone = Console.ReadLine();
+        Console.WriteLine("Введите свой пароль");
+        password = Console.ReadLine();
+        Console.WriteLine("Введите повторно свой пароль");
+        var secondPassword = Console.ReadLine();
+        if(password == secondPassword)
+        {
+            break;
+        }
+    }
+   var user =  userService.CreateUser(phone,password);
+    Console.WriteLine("Вы успешно зарегистрованы");
+    Console.WriteLine("+ - добавить ваши данные");
+    var command = Console.ReadLine();
+    if(command == "+")
+    {
+        AddUserDetails(user);
+    }
+
+}
+void AddUserDetails(User user)
+{
+    Console.WriteLine("Введите ваше имя");
+    user.Name = Console.ReadLine();
+    Console.WriteLine("Введите вашу фамилию");
+    user.LastName = Console.ReadLine();
+    userService.UpdateUser(user);
+}
