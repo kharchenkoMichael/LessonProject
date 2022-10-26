17b486528c3f162782102bdb5b4fdd72abfbf640
ZAMLER <jekaninho7@gmail.com>
2022-10-26 17:25:15 +0300

    #12-#13 - записи

---
+++ ProjectLesson/BussinesLogic/RecordService.cs
@@ -0,0 +1,26 @@
+﻿
+
+using Storage;
+
+namespace BussinesLogic
+{
+    public class RecordService
+    {
+        private DataBase _dataBase;
+        public RecordService()
+        {
+            _dataBase = new DataBase();
+            _dataBase.InitDataBase();
+        }
+        public IEnumerable<Record> GetFutureRecords(User user)
+        {
+
+           return _dataBase.Records.Where(item => item.DateTime > DateTime.Now && item.UserName == user.Name);
+        }
+        public IEnumerable<Record> GetHistoryRecords(User user)
+        {
+
+            return _dataBase.Records.Where(item => item.DateTime < DateTime.Now && item.UserName == user.Name);
+        }
+    }
+}
