14ce1f418f68b7e2ae3f670e6e040c2fa2f5efe4
ZAMLER <112641549+ZAMLER@users.noreply.github.com>
2022-10-31 18:28:01 +0200

    #22- создала Admin

---
+++ ProjectLesson/UserInterface/AdminCommandService.cs
@@ -0,0 +1,38 @@
+﻿
+using Storage;
+
+namespace UserInterface
+{
+    public class AdminCommandService
+    {
+        internal void ApproveRecord(User myUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void CancelRecord(User myUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void CreateNewRecord(string phone)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void ShowAllFutureRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void ShowAllHistoryRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void ShowAllNotApproveRecord(User myUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
