c858f802aaddd32c07fbd6edffb41f1505b3cba3
ZAMLER <112641549+ZAMLER@users.noreply.github.com>
2022-11-26 11:02:41 +0200

    Create Table Procedur

---
+++ ProjectLesson/SQLQuery/SQLQuery2.sql
@@ -0,0 +1,9 @@
+INSERT INTO Procedurs(
+Name,Price,Time)
+values
+('Manikur',250,'1:00:00'),
+('Manikur + Pokruttya',400,'1:30:00'),
+('Pedikur',200,'00:40:00'),
+('Pedikur + Pokruttya',250,'1:30:00'),
+('Ysilenie',100,'00:20:00')
+
